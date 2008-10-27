5dce7f92a880ebf2e7f513b6571b40b956dfeb3d
alessandro <alessandro@gentux.homenet.telecomitalia.it>
2008-10-27 17:35:15 +0100

    base plugin structure

--- experimental/base/akuplugin.cpp
+++ experimental/base/akuplugin.cpp
@@ -1,1 +1,27 @@
- 
+ /* This file is part of the KDE project
+
+   Copyright (C) 2008 Alessandro Diaferia <alediaferia@gmail.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+*/
+#include "akuplugin.h"
+
+class AkuPlugin::AkuPluginPrivate {
+
+public:
+    AkuPluginPrivate(AkuPlugin *q):
+                     q(q)
+                     {}
+
+    AkuPlugin *q;
+};
+
+AkuPlugin::AkuPlugin(QObject *parent) : QObject(parent),
+                                        d(new AkuPluginPrivate(this))
+{}
+
+AkuPlugin::~AkuPlugin()
+{}
--- experimental/base/akuplugin.h
+++ experimental/base/akuplugin.h
@@ -1,1 +1,39 @@
- 
+ /* This file is part of the KDE project
+
+   Copyright (C) 2008 Alessandro Diaferia <alediaferia@gmail.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+*/
+
+#ifndef AKUPLUGIN_H
+#define AKUPLUGIN_H
+
+#include <QObject>
+#include <kdemacros.h>
+
+/** \class AkuPlugin akuplugin.h
+  * \brief Aku's Base Plugin Class for archive management
+  *
+  * This is the base class for every archive plugin in Aku.
+  */
+
+class KDE_EXPORT AkuPlugin : public QObject
+{
+    Q_OBJECT
+
+    public:
+        AkuPlugin(QObject *parent);
+        virtual ~AkuPlugin();
+
+    private:
+        class AkuPluginPrivate;
+        AkuPluginPrivate *d;
+
+// TODO: add convenient methods to share in the main application in
+//       order to correctly retrieve and show infos about the archive.
+};
+
+#endif 
