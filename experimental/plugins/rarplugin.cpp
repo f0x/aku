c832113097fe66b7daf0417ccee300fbd6ec66cf
f0x <francesco@f0x.workgroup>
2008-10-29 18:25:42 +0100

    rar

--- experimental/plugins/rarplugin.cpp
+++ experimental/plugins/rarplugin.cpp
@@ -17,7 +17,10 @@
 
 AKU_PLUGIN_EXPORT(RarPlugin)
 
+// name of the executable
 const QString EXE_NAME = "unrar";
+// the list of files in the rar archive starts after this line
+const QString headerLine = "-------------------------------------------------------------------------------";
 
 RarPlugin::RarPlugin(QObject *parent, const QVariantList &args) : AkuPlugin(parent)
 {}
@@ -37,7 +40,7 @@ bool RarPlugin::canExtract()
 
 bool RarPlugin::canCreate()
 {
-    return true;
+    return false;
 }
 
 bool RarPlugin::canRename()
@@ -54,11 +57,31 @@ void RarPlugin::loadArchive(const KUrl &fileName)
 {
    QProcess *process = new QProcess();
    
-   kDebug() << fileName;
    QStringList options;
-   options << "v" + fileName.pathOrUrl();
+   options << "v" << fileName.pathOrUrl();
    process -> start(EXE_NAME, options);
    process -> waitForFinished();
    QString output;
    output = process -> readAllStandardOutput();
+   kDebug() << output;
+   
+   int indexOfHeaderLine;
+   indexOfHeaderLine = output.indexOf(headerLine);
+   
+   // cut the the text until the end of headerLine
+   output.remove (0, indexOfHeaderLine + 79); 
+   // search for the second headerLine. The list of the file in the archive ends here
+   indexOfHeaderLine = output.indexOf(headerLine);
+   output.remove (indexOfHeaderLine, output.length());
+   output.remove (0, 1);
+   output.remove (output.length() - 1, 1); //others parsing corrections
+   QStringList splitList;
+   splitList = output.split ("\n"); // split at the newline
+   QStringList fileList;
+   for (int i = 0; i < splitList.size(); i++) {
+     if ( i % 2 == 0 ) {
+       fileList << splitList[i].mid(1);
+     }
+   }
+   kDebug() << fileList;
 }
