6d33200154528abdbb232bdc3cff538ebb28fe85
Alessandro Diaferia <alediaferia@gmail.com>
2008-11-16 13:36:52 +0100

    basic extraction, fixes, karchiveutils

---
+++ experimental/plugins/karchiveutils/karchiveutils.h
@@ -0,0 +1,21 @@
+ /*
+
+   Copyright (C) 2008 Alessandro Diaferia <alediaferia@gmail.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+*/
+#ifndef KARCHIVEUTILS_H
+#define KARCHIVEUTILS_H
+
+#include <KArchive>
+
+namespace KArchiveUtils
+{
+    QString formatPermissions(mode_t permissions);
+    
+};
+
+#endif 
